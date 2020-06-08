616f987d46bf54f539f93a00c9b50d3ae0c701b7
Andrew Reist <andrew@builtbyfield.com>
2020-06-08 13:33:24 -0600

    Adjusted messed up page header.

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,6 +39,7 @@ function IndexPage() {
 
       {/* Books */}
       <PageHeader
+        title="James Prower"
         body="The author of 'A Being of Some Cosmic Importance'. James writes Science Fiction and Fantasy novels, and lives in Calgary, Alberta with his wife and daughter. Send any questions, concerns or inquiries to inquiries@jamesprower.com."
         image={homeHeader.childImageSharp.fluid}
       />
