a7b87826a6085495991c365940d962feaae1f923
Andrew Reist <andrew@builtbyfield.com>
2020-06-09 17:57:12 -0600

    Adjusted button to not have overflowing clickable area.

--- src/components/button/src/Button.js
+++ src/components/button/src/Button.js
@@ -2,15 +2,11 @@ import React, { useState } from "react"
 
 import { Box, Heading } from "components"
 
-const Button = ({ link, label, color }) => {
+function Button({ link, label, color }) {
   const [hover, setHover] = useState(false)
   return (
-    <Box
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      css={{ "&.hovered": { opacity: 0.7 } }}
-      className={hover ? "hovered" : ""}
-    >
+    // Wrapper box to keep button's clickable area from overflowing into container.
+    <Box maxWidth="10px">
       <a
         href={link}
         css={{
@@ -19,7 +15,15 @@ const Button = ({ link, label, color }) => {
         target={link.includes("http") ? "_blank" : null}
         rel={link.includes("http") ? "noopener noreferrer" : null}
       >
-        <Heading children={label} color={color} size={400} />
+        <Heading
+          children={label}
+          color={color}
+          size={400}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          css={{ "&.hovered": { opacity: 0.7 } }}
+          className={hover ? "hovered" : ""}
+        />
       </a>
     </Box>
   )
