cbc0d2ea328147ff51f1c1018f6d768dff224e7d
Andrew Reist <andrew@builtbyfield.com>
2020-10-22 14:58:31 -0600

    Update colors.js

--- src/components/theme/src/colors.js
+++ src/components/theme/src/colors.js
@@ -3,13 +3,13 @@ import { rgba } from "polished"
 export default {
   text: "#222",
   alt: rgba("#444444", 0.5),
-  background: "#f1f1f1",
-  wash: "#f2f2f2",
+  background: "#ededed",
+  wash: "#ededed",
   primary: "#FF4D01",
   secondary: "#132048",
   modes: {
     dark: {
-      text: "#f1f1f1",
+      text: "#ededed",
       alt: "#636569",
       background: "#393A3B",
       wash: "#393A3B",
