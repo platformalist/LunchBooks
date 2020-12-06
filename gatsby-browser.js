f764960ebdc77f5f22fbb6248f7d0a53aaee7649
Andrew Reist <andrew@builtbyfield.com>
2020-12-06 10:31:38 -0700

    chore: minor test to see if simple gatsby-browser swap might work

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,19 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import * as React from "react"
 
-// You can delete this file if you're not using it
+const DELAY = 300
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  if (location.action === "PUSH") {
+    window.setTimeout(() => window.scrollTo(0, 0), DELAY)
+  } else {
+    const savedPosition = getSavedScrollPosition(location)
+    window.setTimeout(
+      () => window.scrollTo(...(savedPosition || [0, 0])),
+      DELAY
+    )
+  }
+  return false
+}
